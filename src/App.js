98150779dfc22fbc7d3bb6df7638b255ebf62b59
dixit.soni <dixit.soni@artoon.in>
2024-01-05 06:28:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import DefaultLayout from './layout';
 import MovieDetail from './layout/movieSection/movieDetail/MovieDetail';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename='/movie-app'>
-      <Routes >
+    <HashRouter basename='/movie-app'>
+      <Routes>
         <Route exact path='/movie-app' element={<DefaultLayout />} />
         <Route path='/movie-detail/:id' element={<MovieDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
